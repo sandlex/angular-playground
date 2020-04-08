659c5323229420f8a3e473866010d707991590b6
Alexey Peskov <aspeskov@gmail.com>
2020-04-08 23:14:34 +0200

    Refactoring directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,36 +9,11 @@ export class AppComponent {
 
   private username: string = '';
   private displayDetails: boolean;
-  private logs: LogItem[] = [];
+  private logs: Date[] = [];
 
   onDetailsClick() {
     this.displayDetails = !this.displayDetails;
-    let index = this.logs.length + 1;
-    this.logs.push(new LogItem(index, index >= 5 ? 'blue' : 'white'));
+    this.logs.push(new Date());
   }
 
-}
-
-class LogItem {
-  private index: number;
-  private color: string;
-  private date: Date;
-
-  constructor(index: number, color: string) {
-    this.index = index;
-    this.color = color;
-    this.date = new Date();
-  }
-
-  getIndex(): number {
-    return this.index;
-  }
-
-  getColor(): string {
-    return this.color;
-  }
-
-  getDate(): Date {
-    return this.date;
-  }
 }
