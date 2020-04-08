1fdb46d96d47621519d80ea569416ae144888b19
Alexey Peskov <aspeskov@gmail.com>
2020-04-08 23:08:55 +0200

    Playing with directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +8,37 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   private username: string = '';
+  private displayDetails: boolean;
+  private logs: LogItem[] = [];
+
+  onDetailsClick() {
+    this.displayDetails = !this.displayDetails;
+    let index = this.logs.length + 1;
+    this.logs.push(new LogItem(index, index >= 5 ? 'blue' : 'white'));
+  }
+
+}
+
+class LogItem {
+  private index: number;
+  private color: string;
+  private date: Date;
+
+  constructor(index: number, color: string) {
+    this.index = index;
+    this.color = color;
+    this.date = new Date();
+  }
+
+  getIndex(): number {
+    return this.index;
+  }
+
+  getColor(): string {
+    return this.color;
+  }
+
+  getDate(): Date {
+    return this.date;
+  }
 }
