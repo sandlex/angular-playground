c6750818db9bbd8d8851d39aab76345f005330ba
Alexey Peskov <aspeskov@gmail.com>
2020-09-07 21:56:25 +0200

    Making menu clickable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,4 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  selectedMenu: string = 'recipes';
+
+  onNavigate(selectedMenu: string) {
+    this.selectedMenu = selectedMenu;
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  @Output() selectedMenu = new EventEmitter<string>();
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
 
+  onClick(selectedMenu: string) {
+    this.selectedMenu.emit(selectedMenu);
+  }
 }
