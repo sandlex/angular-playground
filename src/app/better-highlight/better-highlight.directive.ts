b02bf834c1d0826ae42aa28d406dbe9270c06434
Alexey Peskov <aspeskov@gmail.com>
2020-09-14 09:53:34 +0200

    Reacting to host events

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -1,4 +1,4 @@
-import {Directive, ElementRef, OnInit, Renderer2} from "@angular/core";
+import {Directive, ElementRef, HostListener, OnInit, Renderer2} from "@angular/core";
 
 @Directive({
   selector: '[appBetterHighlight]'
@@ -10,5 +10,13 @@ export class BetterHighlightDirective implements OnInit {
 
   ngOnInit() {
     this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'blue');
+  }
+
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent');
   }
 }
