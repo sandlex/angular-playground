e4b981f2d1abedb49d5c902d33e6e9b23768f606
Alexey Peskov <aspeskov@gmail.com>
2020-10-30 10:34:46 +0100

    Cross component communication using services and events

--- src/app/another.service.ts
+++ src/app/another.service.ts
@@ -1,8 +1,10 @@
 import {LoggingService} from "./logging.service";
-import {Injectable} from "@angular/core";
+import {EventEmitter, Injectable} from "@angular/core";
 
 @Injectable()
 export class AnotherService {
+
+  onSomeEvent = new EventEmitter<string>();
 
   constructor(private loggingService: LoggingService) {
   }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ export class AppComponent {
     comment: 'comment'}];
 
   constructor(private loggingService: LoggingService, private anotherService: AnotherService) {
+    this.anotherService.onSomeEvent.subscribe((value:string) => alert(value))
   }
 
   onServerAdded(serverData: {serverName: string, serverContent: string, serverComment: string}) {
@@ -36,6 +37,7 @@ export class AppComponent {
     });
     this.loggingService.logRecord('added blueprint');
     this.anotherService.doSomething();
+    this.anotherService.onSomeEvent.emit('Boo!');
   }
 
 }
